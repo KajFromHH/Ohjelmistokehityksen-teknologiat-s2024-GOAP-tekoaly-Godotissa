93dc13d90ee2ac58805db798005b63a16cdb8f9f
Kaj Jansson <kaj.jansson@myy.haaga-helia.fi>
2024-12-01 21:46:34 +0200

    Fixed radius of collision (9px, which makes diameter 18 px -> ok).

--- goap-godot-ohke/Scenes/Beliefs.cs
+++ goap-godot-ohke/Scenes/Beliefs.cs
@@ -29,13 +29,57 @@ public partial class AgentBeliefs : Node
 	//Olennainen määritelmä, kun Simon täytyy uudelleenarvioida ympäristöään.
 	public Vector2I Location => observedLocation();
 
+	//Lisätään konstruktoori, joka hyväksyy vain nimen.
 	AgentBeliefs(string name)
 	{
 		Name = name;
 	}
 
+	//Uudelleenrvioidaan tilannetta ja ympäristöä,
+	//jos nykyinen tapahtuma on totta vai ei.
 	public bool Evaluate()
 	{
 		return condition();
 	}
+
+	//Lisätään uskomuksille rakennetta, ts. Builder.
+
+	public class Builder
+	{
+		readonly AgentBeliefs belief;
+
+		//Hyväksytään uskomuksen nimen
+		//ja luodaan uuden version.
+		public Builder(string name)
+		{
+			belief = new AgentBeliefs(name);
+		}
+
+		//Lisätään tilanne kyseiselle uskomukselle.
+		public Builder WithCondition(Func<bool> condition)
+		{
+			belief.condition = condition;
+			return this;
+		}
+
+		//Lisätään uskomuksen paikkakoordinaatit.
+		public Builder WithLocation(Func<Vector2I> observedLocation)
+		{
+			belief.observedLocation = observedLocation;
+			return this;
+		}
+
+		//Rakennetaan loppultaan kyseisen uskomuksen. 
+		public AgentBeliefs Build()
+		{
+			return belief;
+
+		}
+	}
 }
+
+//Luodaan uskomuksille tehtaan, jotta
+//pystymme hallinoimaan helpommin monia uskomuksia.
+
+//Tehtaassa sisältää metodeja ja informaatiota,
+//jotka muodostavat sanakirjan (Dictionary) uskomuksille.
